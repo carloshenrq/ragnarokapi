70770586e071edba1786b2487d6e5f09eab6e52c
Carlos Henrique <carloshlfzanon@gmail.com>
2019-08-23 10:06:29 -0300

    Added new tests.

--- tests/LoginRegNumModelTest.php
+++ tests/LoginRegNumModelTest.php
@@ -24,6 +24,44 @@ class LoginRegNumModelTest extends PHPUnit\Framework\TestCase
 {
     use TDatabaseSetup;
 
+    public function testLoginRegNumFind()
+    {
+        // Cria uma entrada no banco de dados
+        Model\LoginRegNum::create([
+            'account_id' => 1,
+            'key' => '#RAGNAROKAPI',
+            'index' => 0,
+            'value' => 1000
+        ]);
+
+        $regNum = Model\LoginRegNum::find([
+            'account_id' => 1,
+            'key' => '#RAGNAROKAPI',
+            'index' => 0
+        ]);
+        $this->assertNotNull($regNum);
+        $this->assertInstanceOf('Model\LoginRegNum', $regNum);
+        $this->assertEquals(1, $regNum->account_id);
+        $this->assertEquals('#RAGNAROKAPI', $regNum->key);
+        $this->assertEquals(0, $regNum->index);
+        $this->assertEquals(1000, $regNum->value);
+
+        $regNum->delete();
+    }
+
+    public function testLoginRegNumCreate()
+    {
+        $regNum = new Model\LoginRegNum;
+        
+        $regNum->account_id = 1;
+        $regNum->key = '#RAGNAROKAPI';
+        $regNum->index = 0;
+        $regNum->value = 1000;
+
+        $regNum->save();
+        $regNum->delete();
+    }
+
     public function testLoginRegNum()
     {
         // Cria uma entrada no banco de dados
